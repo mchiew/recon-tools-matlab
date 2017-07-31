389928269aca383b732a954f8739433cc5093f6f
Mark Chiew <mchiew@fmrib.ox.ac.uk>
2017-07-31 16:46:02 +0100

    Cleaned up fista implementation

--- fista/fista.m
+++ fista/fista.m
@@ -1,4 +1,4 @@
-function [u,M] = fista(d, xfm, W, lambda, im_size, niter, step)
+function u = fista(d, xfm, W, lambda, im_size, niter, step)
 
 %   Mark Chiew  
 %   May 2017
@@ -11,8 +11,17 @@ function [u,M] = fista(d, xfm, W, lambda, im_size, niter, step)
 %   min{u} |xfm*u - d|_2 + lambda*|W*u|_1
 %
 %   where u is the estimate,  W is an invertible  transform (often Wavelet), 
+%   or identity transform
 %   xfm is the measurement operator, lambda is the L1-weighting
 %   and d is the measured raw k-space data
+
+%   To make things easier, we work with our estimate u in the main loop
+%   in the sparse domain, so that our measurement operator is actually 
+%   effectively xfm*W'
+%   Then we simply transform by W' as a final step
+%   
+%   Another way of viewing this is that we perform a change of variables
+%   such that u_new = W*u
 
 %   Constant stepsize variant
 
@@ -24,9 +33,12 @@ function [u,M] = fista(d, xfm, W, lambda, im_size, niter, step)
 
 %   Main loop
 for ii = 1:niter
+
+    %   Data consistency
+    u   =   v + step*(W*(xfm'.*(d - xfm*(W'*v))));
+
     %   Solve proximal sub-problem
-    u   =   shrink(v - (1./(1+(ii*step).^.25))*(W*(xfm'.*(xfm*(W'*v)-d))),lambda*step);    
-    %u   =   shrink(v - step*(W*(xfm'.*(xfm*(W'*v)-d))),lambda*step);    
+    u   =   shrink(u, lambda*step);
 
     %   Compute momentum parameter
     t2  =   (1+sqrt(1+4*t^2))/2;
@@ -37,10 +49,6 @@ for ii = 1:niter
     %   Update variables
     t   =   t2;
     up  =   u;
-
-    show(abs(W'*u)',[]);
-    text(2,6,sprintf('%03d',ii),'color','w','FontSize',64);
-    M(ii)=getframe;
 end
 
 u   =   W'*u;
