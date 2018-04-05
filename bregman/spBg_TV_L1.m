d7b9910fb4a613cff602d2fe64ee45c5120330d5
Mark Chiew <mchiew@fmrib.ox.ac.uk>
2018-04-05 17:01:24 +0100

    Uses mtimes2/Toeplitz multiplication now

--- bregman/spBg_TV_L1.m
+++ bregman/spBg_TV_L1.m
@@ -1,4 +1,4 @@
-function u = spBg_TV_L1(d, xfm, W, im_size, niter, mu, lambda, gamma)
+function u = spBg_TV_L1(d, xfm, W, niter, mu, lambda, gamma)
 
 %   Mark Chiew  
 %   Jan 2017
@@ -18,7 +18,7 @@ function u = spBg_TV_L1(d, xfm, W, im_size, niter, mu, lambda, gamma)
 
 
 niter_outer  =   niter;
-niter_inner  =   50;
+niter_inner  =   5;
 
 if nargin < 6
 mu      =   1;
@@ -31,12 +31,12 @@ gamma   =   mu/10;
 end
 
 %   Normalise mu, lambda and gamma relative to norm(d)
-mu      =   mu*norm(d);
-lambda  =   lambda*norm(d);
-gamma   =   gamma*norm(d);
+%mu      =   mu*norm(d);
+%lambda  =   lambda*norm(d);
+%gamma   =   gamma*norm(d);
 
 %   Initialise variables
-u   =   zeros(im_size);
+u   =   zeros([xfm.Nd(1:3) xfm.Nt]);
 dx  =   g(u,-1,1);
 dy  =   g(u,-1,2);
 w   =   W*u;
@@ -45,40 +45,16 @@ by  =   0;
 bw  =   w; 
 f   =   d;
 
-%   Pre-compute helper matrix to solve the inner u-subproblem
-%   Note that this is approximate in some cases because we're using the FFT to 
-%   speed up the solution of the Afun-system, but it is only exact when the 
-%   data are sampled on a Cartesian grid (i.e. FFT-mode in xfm)
-%   For non-uniform sampling (i.e. NUFFT-mode in xfm), this fast exact
-%   solution isn't practical, but we just use an approximate form 
-%   Alternatively, for precision, you can use the symmlq solver commented
-%   out below
-A   =   zeros(im_size);
-A(1,1)  =   4;
-A(1,2)  =   -1;
-A(2,1)  =   -1;
-A(end,1)=   -1;
-A(1,end)=   -1;
-if strcmp(xfm.mode, 'NUFFT')
-    A   =  mu + lambda*fftshift(fftshift(fft2(A),1),2) + gamma;
-else
-    A   =  lambda*fftshift(fftshift(fft2(A),1),2) + gamma;
-    A(xfm.mask) = A(xfm.mask) + mu; 
-end
-
 %   Main loops
 for i  = 1:niter_outer
 for ii = 1:niter_inner
    
     %   Solve u-subproblem
-    %   Can use symmlq because Afun is symmetric
-    %   This is slow because it's indirect, but it's technically more precise
-    %[u,~,relres]=  reshape(symmlq(@(x,mode) Afun(reshape(x, im_size), xfm, mu, lambda, gamma),...
-    %               reshape(mu*(xfm'.*f) + lambda*(g(dx-bx,1,1) + g(dy-by,1,2)) + gamma*(W'*(w-bw)),[],1),...
-    %               1E-9, 500, [], [], u(:)), im_size);
+    [u,~,relres]=  minres(@(x) reshape(mu*mtimes2(xfm,x),[],1) + reshape(lambda*(g(g(reshape(x,[xfm.Nd xfm.Nt]),-1,1),1,1) + g(g(reshape(x,[xfm.Nd xfm.Nt]),-1,2),1,2)),[],1) + gamma*x,...
+                   reshape(mu*(xfm'.*f) + lambda*(g(dx-bx,1,1) + g(dy-by,1,2)) + gamma*(W'*(w-bw)),[],1),...
+                   1E-6, 100, [], [], u(:));
+    u           =   reshape(u, [xfm.Nd(1:3) xfm.Nt]);
 
-    %   Solve u-subproblem
-    u   =   ifft2c(fft2c(mu*(xfm'.*f) + lambda*(g(dx-bx,1,1) + g(dy-by,1,2)) + gamma*(W'*(w-bw)))./A);
 
     %   Update auxilliary variables
     s   =   sqrt(abs(g(u,-1,1) + bx).^2 + abs(g(u,-1,2) + by).^2);
@@ -91,18 +67,12 @@ for ii = 1:niter_inner
 end
     %   Update fidelity term for outer loop
     f   =   f + d - xfm*u;
-    figure(100);show(abs(u),[]);
-    fprintf(1, 'Iter: %03d, Objective: %f, DataConsistency: %f\n', i, objective(f, u, xfm, W, mu, lambda, gamma)/norm(d).^2, norm(reshape(xfm*u-d,[],1)));
+    fprintf(1, 'Iter: %03d, Objective: %f, DataConsistency: %f\n', i, objective(d, u, xfm, W, mu, lambda, gamma), (mu/2)*sum(reshape(abs(xfm*u-d).^2,[],1)));
 end
 
 %   The main objective function
 function res = objective(d, x, xfm, W, mu, lambda, gamma)
     res =   lambda*(sqrt(L1(g(x,-1,1)).^2 + L1(g(x,-1,2)).^2)) + gamma*L1(W*x) + (mu/2)*sum(reshape(abs(xfm*x-d).^2,[],1));
-
-%   Matrix for solving the u-subproblem
-function res = Afun(x, E, mu, lambda, gamma)
-    res =   mu*(E'.*(E*x)) + lambda*(g(g(x,-1,1),1,1) + g(g(x,-1,2),1,2)) + gamma*x;
-    res =   reshape(res,[],1);
 
 %   L1-norm
 function res = L1(input)
