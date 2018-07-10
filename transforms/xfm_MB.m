45757cf2654de4e6bf0c7dafc5ab2ccdd4d114f3
Mark Chiew <mchiew@fmrib.ox.ac.uk>
2018-07-10 16:57:04 +0100

    Further optimisation of bias_var method

--- transforms/xfm_MB.m
+++ transforms/xfm_MB.m
@@ -216,9 +216,9 @@ function [d, g] = bias_var(a,L)
     R2      =   sparse(toeplitz(reshape(padarray(L*[6 -4 1 zeros(1,a.Nt-5) 1 -4],[prod(a.Nd(2:3))-1 0],'post'),[],1)));
     
     for s = 1:a.Ns
-    fprintf(1,'\n000');
+    %fprintf(1,'\n000');
     for i = 1:a.Nd(1)   
-        fprintf(1,'\b\b\b%03d',i);        
+        %fprintf(1,'\b\b\b%03d',i);        
         iy      =   [];
         iz      =   [];
         dd      =   [];
@@ -256,25 +256,23 @@ function [d, g] = bias_var(a,L)
                     qL  =   length(qq);
                     qq  =   find(sum(B*B(:,qq),2));
                 end
-                if length(qq) == length(ind)
-                    BB = full(B(qq,qq));
-                else
-                    BB = B(qq,qq);
-                end
-                C   =   BB\A(qq,qq);
+                BB  =   inv(B(qq,qq));
+                %C   =   BB\A(qq,qq);
+                C   =   BB*A(qq,qq);
                 
                 d(rdx(qq),i)    =   real(diag(C));
                 
-                C   =   C/BB;
-                
-                g(rdx(qq),i)    =   real(sqrt(diag(C).*diag(A(qq,qq))));     
+                %C   =   C/BB;
+                C   =   sum(C.*BB',2);
+                %g(rdx(qq),i)    =   real(sqrt(diag(C).*diag(A(qq,qq))));     
+                g(rdx(qq),i)    =   real(sqrt(C.*diag(A(qq,qq))));     
                 
                 ind =   setdiff(ind,qq);
             end
         end
             
     end
-    fprintf(1,'\n');    
+    %fprintf(1,'\n');    
     end
     d   =   reshape(d.', [a.Nd, a.Nt]);
     d   =   d(:,:,:,1);
